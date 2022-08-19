8fa1da3d1febfe15dc0adc9d62037bb9dd264d28
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-08-19 04:41:31 -0700

    Update trial1.py

--- week1/fiz-buz/trial1.py
+++ week1/fiz-buz/trial1.py
@@ -1,1 +1,12 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        list=[]
+        if n%3==0:
+            list=list+["Fizz"]
+        elif n%5:
+            list=list+["buzz"]
+        elif n%15:
+            list=list+["FizzBuzz"]
+        else:
+            list=list+[n]
 
