e3dd9041f697bca3e06cb2054aeabe52201f7dce
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-08-21 05:37:47 -0700

    Update trial2.py

--- week1/sorting-sentence/trial2.py
+++ week1/sorting-sentence/trial2.py
@@ -1,1 +1,17 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        list=s.split
+        for i in range(len(list)):
+            list[i][-1]
+            for j in range(len(list)):
+                list[j][-1]
+                if list[i][-1]>list[j][-1]:
+                    d=list[i][-1]
+                    list[i][-1]=list[j][-1]
+                    list[j][-1]=d
+        for i in range(len(list)):
+            s=list[i][:len(list[i])-1].join
+        return s
+a=Solution()
+a.sortSentence("abc2 kj1 dkd3 ds5 dsa4")
 
