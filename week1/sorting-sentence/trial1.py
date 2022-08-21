f72d6084c97307a278332bef82baaec032952a22
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-08-21 05:11:23 -0700

    Update trial1.py

--- week1/sorting-sentence/trial1.py
+++ week1/sorting-sentence/trial1.py
@@ -1,1 +1,16 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        list=s.split()
+        for i in range(len(list)):
+            list[i][-1]
+            for j in range(len(list)):
+                list[j][-1]
+                if list[i][-1]>list[j][-1]:
+                    d=list[i][-1]
+                    list[i][-1]=list[j][-1]
+                    list[j][-1]=d
+        for i in range(len(list)):
+            s=list[i][:len(list[i])-1].join()
+        return s
+
 
