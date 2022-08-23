2cbabb56d0dedd066a6b8301f5d5f3d839397a40
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-08-23 05:27:17 -0700

    Create trial5.py

---
+++ week1/array-with-elements-average/trial5.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(1,len(nums)-1):
+            if (nums[i-1]+nums[i+1])/2==nums[i]:
+                b=nums[i+1]
+                nums[i+1]=nums[i]
+                nums[i]=b
+        for i in range(len(nums)-2,0,-1):
+                if (nums[i-1]+nums[i+1])/2==nums[i]:
+                    b=nums[i-1]
+                    nums[i-1]=nums[i]
+                    nums[i]=b
+                    
+        return nums
+        
