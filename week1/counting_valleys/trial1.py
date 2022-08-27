89409458394eb698ec17cbc8c8d1715d73d00ced
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-08-27 12:13:06 -0700

    Create trial1.py

---
+++ week1/counting_valleys/trial1.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    some=[]
+    count=0
+    path_count=0
+    for i in range(len(path)):
+        if path[i]=='U':
+            path_count+=1
+        else:
+            path_count-=1
+        some=some+[path_count]
+    for i in range(len(some)-1):
+        if some[i]==-1 and some[i+1]==0:
+            count+=1
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
