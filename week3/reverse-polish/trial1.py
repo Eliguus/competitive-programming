478131de95218ddf11390c93c057b345db99edc8
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-09-12 12:27:14 -0700

    Create trial1.py

---
+++ week3/reverse-polish/trial1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        ans=[]
+        
+        for i in tokens:
+            if i=='+':
+                ans.append(ans.pop() + ans.pop())
+                
+            elif i=='-':
+                ans.append(ans.pop() - ans.pop())
+                
+            elif i=='*':
+                ans.append(ans.pop() * ans.pop())
+                
+            elif i=='/':
+                var1=ans.pop()
+                var2=ans.pop()
+                
+                ans.append(int(var2/var1))
+                
+            else:
+                ans.append(int(i))
+            
+            
+                
+        return ans[0]
+            
+        
