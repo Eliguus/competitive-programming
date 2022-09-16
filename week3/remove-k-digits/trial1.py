c07b786b73e1422e5abd0d2ed70e20134017bb22
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-09-16 10:33:31 -0700

    Create trial1.py

---
+++ week3/remove-k-digits/trial1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if len(num)==k:
+            return '0'
+        
+        stack=[]
+        for i in range(len(num)):
+            
+            while k>0 and stack and num[i]<stack[-1]:
+                k-=1
+                stack.pop()
+            stack.append(num[i])
+            
+        if k>0:
+            for i in range(k):
+                stack.pop()
+        if stack[0]=='0':
+            while stack[0]=='0':
+                stack.pop(0)
+            
+                
+            
+        return ''.join(stack)
+            
+        
