90c782eac2e5ed2c3605add34662d9ce173bc6ca
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-09-16 08:11:19 -0700

    Create trial2.py

---
+++ week3/daily-temperature/trial2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        days=[0]*len(temperatures)
+        stack=[]
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]]<temperatures[i]:
+                ind=stack.pop()
+                days[ind]=i-ind
+                
+            stack.append(i)
+                
+        return days
+                    
+                    
+                
+                
+                
+        
