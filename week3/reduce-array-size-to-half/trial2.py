caced0480977628960e175d0011ab778156150bd
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-09-17 08:45:28 -0700

    Create trial2.py

---
+++ week3/reduce-array-size-to-half/trial2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        count=Counter(arr)
+        total=0
+        lis=[]
+        for value,fre in count.items():
+            total+=fre
+            lis+=[[fre,value]]
+        sort_lis=sorted(lis)
+        fre_sum=sort_lis[-1][1]
+        sort_lis.pop()
+        if len(sort_lis)==0:
+            return 1
+        while fre_sum<len(arr)/2:
+            fre_sum=sort_lis[-1][1]
+            sort_lis.pop()
+        return len(sort_lis)
+        
