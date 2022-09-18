36940e1f98e320c3a19d1ce96e5762df8597a740
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-09-18 10:53:16 -0700

    Create trial2.py

---
+++ week3/power-of-three/trial2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n==3 or n==1:
+            return True
+        if n<3:
+            return False
+        else:
+            n=n/3
+            return self.isPowerOfThree(n)
+        
