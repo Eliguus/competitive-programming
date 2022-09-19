3455fe175ff2243de4152597b7b450638f6a9417
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-09-19 10:36:50 -0700

    Create trial2.py

---
+++ week4/find-the-duplicate-num/trial2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums=Counter(nums)
+        for key,value in nums.items():
+            if value>=2:
+                return key
+        
+        
