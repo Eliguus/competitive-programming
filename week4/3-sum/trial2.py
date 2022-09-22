68895c6e599c7e0b6d15d742eb16b9888503f9ac
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-09-22 04:47:26 -0700

    Create trial2.py

---
+++ week4/3-sum/trial2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        m=0
+        nums=sorted(nums)
+        ans=[]
+        for m in range(len(nums)):
+            l=m+1
+            r=len(nums)-1
+            while l<r:
+                if l==m:
+                    l+=1
+                    
+                elif r==m:
+                    r-=1
+                    
+                elif nums[l]+nums[m]+nums[r]==0:
+                    
+                    if  sorted([nums[r],nums[m],nums[l]]) not in ans:
+                        ans+=[sorted([nums[l],nums[m],nums[r]])]
+                    l+=1
+                    r-=1
+                elif nums[l]+nums[m]+nums[r]>0:
+                    r-=1
+                elif nums[l]+nums[m]+nums[r]<0:
+                    l+=1
+            
+        
+        return ans
+                    
+        
