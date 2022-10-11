5a7209debe273872075e2543f65846c5bf69ac7b
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2022-10-11 11:45:24 -0700

    Create trial2.py

---
+++ week7/corporate-flight-bookings/trial2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        ans=[0]*n
+        for first,last,seats in bookings:
+            ans[first-1]+=seats
+            if last<n: ans[last]-=seats
+        for j in range(1,n):
+                ans[j]+=ans[j-1]
+        return ans
