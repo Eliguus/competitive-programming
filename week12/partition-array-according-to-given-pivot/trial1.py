855c0a7c17ad33ee560603c447c826c35617c87e
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2023-01-10 06:49:03 -0800

    Create trial1.py

---
+++ week12/partition-array-according-to-given-pivot/trial1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less=[]
+        more=[]
+        equal=[]
+        for num in nums:
+            if num>pivot:
+                more.append(num)
+            elif num<pivot:
+                less.append(num)
+            else:
+                equal.append(num)
+        nums=less+equal+more
+        return nums
