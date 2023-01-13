cad26d304097bb410b51fc35ddb3990eb5c44578
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2023-01-13 07:03:20 -0800

    Create trial1.py

---
+++ week12/find-winner-of-circuar-game/trial1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        
+        stack=[i for i in range(n,0,-1)]
+
+        while len(stack)!=1:
+            for i in range(1,k):
+                
+                stack.insert(0,stack.pop())
+            stack.pop()
+        return stack[0]
+        
+        
