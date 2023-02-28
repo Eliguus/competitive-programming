dc99f6fe23ba2eb0dc4139a895deb576244c19a1
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2023-02-28 06:51:44 -0800

    Create trial1.py

---
+++ week18/array-manipulation/trial1.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    arr = [0]*(n)
+    for ran in queries:
+        arr[ran[0]-1]+=ran[2]
+        if ran[1]<n:
+            arr[ran[1]]-=ran[2]
+    for idx in range(n-1):
+        arr[idx+1]+=arr[idx]
+    
+    return max(arr)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
