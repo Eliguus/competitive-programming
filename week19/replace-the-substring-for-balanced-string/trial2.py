68ce1dbc65594ee765bd1fa0ae4bf55f833800f7
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2023-03-10 10:49:08 -0800

    Create trial2.py

---
+++ week19/replace-the-substring-for-balanced-string/trial2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        
+        n = len(s)
+        left, ans, c = 0, n, Counter(s) 
+        
+        for right in range(n):                               
+            c[s[right]] -= 1                                
+
+            while left < n and 4*max(c.values()) <= n: 
+
+                ans = min(ans, right-left+1)                
+                c[s[left]] += 1 
+                left += 1
+
+        return ans
