266a046544709d8b80b93e21fb8b2d565585a314
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2023-07-26 22:09:44 +0300

    Create Cirno's Perfect Bitmasks Classroom.py

---
+++ Cirno's Perfect Bitmasks Classroom.py
@@ -0,0 +1,17 @@
+n = int(input())
+for _ in range(n):
+    x = int(input())
+
+    if x ==1:
+        print(3)
+        continue
+    i = 0
+    y = 0
+    while y<=0:
+        y = x & (1<<i)
+        i +=1
+
+    if (y ^ x) == 0:
+        print(y+1)
+    else:
+        print(y)
