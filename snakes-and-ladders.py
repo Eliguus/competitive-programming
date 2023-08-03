3c408c9f8fc504278cb8544b4a652b0ea04da418
Naol Taye <105100708+Eliguus@users.noreply.github.com>
2023-08-03 17:40:12 +0300

    added snakes-and-ladders.python

---
+++ snakes-and-ladders.py
@@ -0,0 +1,32 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:  
+        n = len(board)
+        jumps, target = {}, n * n
+
+        board.reverse()
+        for i in range(n):  
+            if i % 2:
+                board[i].reverse() 
+            else: 
+                None
+
+        c = 0
+        for i in board:
+            for j in i:
+                c += 1
+                if j == -1: continue
+                jumps[c] = j
+        
+        q, seen = deque([(1, 0)]), set([1])
+
+        while q:
+            i, moves = q.popleft()
+            if i == target: return moves
+            for j in range(i+1, i+7):
+                if j > target: break
+                if j in seen: continue
+                seen.add(j)
+                if j in jumps: j = jumps[j]
+                q.append((j, moves + 1))
+
+        return -1
